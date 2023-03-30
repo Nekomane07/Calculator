de79d3c4b0b1fc18bf25f42a40382073b34daab4
Nekomane07 <113617933+Nekomane07@users.noreply.github.com>
2023-03-30 00:58:43 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+// Allows the user to solve using Enter key on Keyboard
+var input = document.getElementById("input");
+
+// auto selects the display for the user to type in on page startup
+window.onload = function() {
+  document.getElementById("input").focus();
+};
+
+// Execute a function when the user presses a key on the keyboard
+input.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault(); // Prevents page from refreshing
+    try {
+      eval(document.getElementById('input').value)
+      document.getElementById("equivalent").click();
+    }
+    catch(err) {
+      document.getElementById("input").value =
+      'ERROR';
+    }
+    
+  }
+});
+
+document.getElementById('equivalent').onclick = function() {
+  
+  try {
+    document.getElementById('input').value = eval(document.getElementById('input').value)
+  }
+  catch(err) {
+    document.getElementById("input").value =
+    'ERROR';
+  }
+
+}​;
